e5a62dca21e298a7ba82e51642d7e4235d973334
KdGaming0 <97355992+KdGaming0@users.noreply.github.com>
2025-06-20 08:15:03 +0200

    initialize release

---
+++ src/main/java/com/github/scaleme/Scaleme.java
@@ -0,0 +1,10 @@
+package com.github.scaleme;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Scaleme implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/com/github/scaleme/client/ScalemeClient.java
@@ -0,0 +1,10 @@
+package com.github.scaleme.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ScalemeClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
