3fc7481b468094b6c46de773f04a9565dd524793
KdGaming0 <97355992+KdGaming0@users.noreply.github.com>
2025-08-13 18:20:46 +0200

    Refactor scaling configuration and improve descriptions in settings

--- src/main/java/com/github/kd_gaming1/scaleme/config/ScaleMeConfig.java
+++ src/main/java/com/github/kd_gaming1/scaleme/config/ScaleMeConfig.java
@@ -4,7 +4,6 @@ import eu.midnightdust.lib.config.MidnightConfig;
 
 public class ScaleMeConfig extends MidnightConfig {
     public static final String SCALING = "scaling";
-    public static final String PLAYERS = "players";
     public static final String VIEW = "view";
 
     // Own Player Scaling
@@ -14,18 +13,17 @@ public class ScaleMeConfig extends MidnightConfig {
     @Entry(category = SCALING, name = "Own Player Scale", isSlider = true, min = 0.1f, max = 3.0f)
     public static float ownPlayerScale = 1.0f;
 
-    @Entry(category = SCALING, name = "Smooth Scaling")
-    public static boolean smoothScaling = true;
-
-    // Players (Other Players + Presets)
-    @Comment(category = PLAYERS, name = "Adjust the visual size of other players and manage presets")
+    @Comment(category = SCALING, name = "Adjust the visual size of other players")
     public static Comment playersDescription;
 
-    @Entry(category = PLAYERS, name = "Enable Scaling for Other Players")
+    @Entry(category = SCALING, name = "Enable Scaling for Other Players")
     public static boolean enableOtherPlayersScaling = false;
 
-    @Entry(category = PLAYERS, name = "Other Players Scale", isSlider = true, min = 0.1f, max = 3.0f)
+    @Entry(category = SCALING, name = "Other Players Scale", isSlider = true, min = 0.1f, max = 3.0f)
     public static float otherPlayersScale = 1.0f;
+
+    @Comment(category = SCALING, name = "Adjust the visual size of NPC players")
+    public static Comment npcDescription;
 
     @Entry(category = SCALING, name = "Enable Hypixel NPC Scaling")
     public static boolean enableNpcScaling = false;
@@ -33,13 +31,23 @@ public class ScaleMeConfig extends MidnightConfig {
     @Entry(category = SCALING, name = "NPC Scale", isSlider = true, min = 0.1f, max = 3.0f)
     public static float npcPlayerScale = 1.0f;
 
+    @Comment(category = SCALING, name = "Adjust the visual size of Villager NPC Scaling")
+    public static Comment villagerNpcDescription;
+
     @Entry(category = SCALING, name = "Enable Hypixel Villager NPC Scaling")
     public static boolean enableVillagerNpcScaling = false;
 
     @Entry(category = SCALING, name = "Villager NPC Scale", isSlider = true, min = 0.1f, max = 3.0f)
     public static float villagerNpcScale = 1.0f;
 
+    @Comment(category = SCALING, name = "Make changes between scale levels appear smooth")
+    public static Comment smoothedScalingDescription;
+
+    @Entry(category = SCALING, name = "Smooth Scaling")
+    public static boolean smoothScaling = true;
+
     /*
+    TODO: Add Quick Add Button to chat behind players
     @Entry(category = PLAYERS, name = "Show Quick Add Button")
     public static boolean showQuickAddButton = true;
     */
